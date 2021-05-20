a5849951050de8770120bee1c1067abde4341a87
Mohammad Raufzahed <mohammadraufzahed@protonmail.com>
2021-05-20 15:32:36 +0430

    Update the SkillsBox component

--- src/components/Home/component.js
+++ src/components/Home/component.js
@@ -1,8 +1,28 @@
-import React from "react";
+import { React, useState } from "react";
 import SliderBox from "./components/SliderBox/SliderBox";
 import SkillsBox from "./components/SkillsBox/SkillsBox";
 
 const Home = () => {
+  const [skillsObj] = useState([
+    {
+      title: "Python",
+      description: "I can write a beutifull apps with python",
+      rate: "5",
+      fontAwesomeIcon: "fab fa-python",
+    },
+    {
+      title: "PHP",
+      description: "I can write a beutifull web apps with php",
+      rate: "2",
+      fontAwesomeIcon: "fab fa-php",
+    },
+    {
+      title: "Linux",
+      description: "I'm in love with Gnu/Linux",
+      rate: "3",
+      fontAwesomeIcon: "fab fa-linux",
+    },
+  ]);
   return (
     //   Home component
     <div>
@@ -13,7 +33,7 @@ const Home = () => {
             name="Mohammad Raufzahed"
             about="a junior Backend programmer"
           />
-          <SkillsBox />
+          <SkillsBox SkillsObject={skillsObj} />
         </div>
       </div>
     </div>
--- src/components/Home/components/SkillsBox/SkillsBox.js
+++ src/components/Home/components/SkillsBox/SkillsBox.js
@@ -1,33 +1,12 @@
 import { React } from "react";
 import SkillBox from "./components/SkillBox/SkillBox";
-const SkillsBox = () => {
-  let skillsObj = [
-    {
-      title: "Python",
-      description: "I can write a beutifull apps with python",
-      rate: "5",
-      fontAwesomeIcon: "fab fa-python",
-    },
-    {
-      title: "PHP",
-      description: "I can write a beutifull web apps with php",
-      rate: "2",
-      fontAwesomeIcon: "fab fa-php",
-    },
-    {
-      title: "Linux",
-      description: "I'm in love with Gnu/Linux",
-      rate: "3",
-      fontAwesomeIcon: "fab fa-linux",
-    },
-  ];
-
+const SkillsBox = ({ SkillsObject }) => {
   return (
     <div className="container is-fullhd has-text-centered mt-5">
       <h2 className="title is-size-2">Skills</h2>
       <span className="subtitle">some of my skills</span>
       <div className="columns is-multiline pt-5">
-        {skillsObj.map((skill, key) => {
+        {SkillsObject.map((skill, key) => {
           return <SkillBox skillObj={skill} key={key} />;
         })}
       </div>
